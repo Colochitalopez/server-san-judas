4e558c635bd3902f0b3c9001b215640bcb4627ec
Darling Prado <darlingprado@sanjudastadeo.edu.gt>
2025-09-16 15:57:32 -0600

    16/09/2025

--- configs/app.js
+++ configs/app.js
@@ -6,19 +6,31 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 
 import 'dotenv/config';
+import { dbConnection } from './db.js'
+import 'dotenv/config';
 
 const middlewares = (app)=>{
     app.use(express.json());
-    app.use(express.urlencoded({extended: false}))
+    app.use(express.urlencoded({extended: false}));
     app.use(cors());
     app.use(helmet());
     app.use(morgan('dev'));
 }
 
-export const initServer =()=> {
+const conectarDB = async () => {
+    try{
+        await dbConnection();
+    }catch(error){
+        console.log(`Error al conectar la db: ${error.message}`)
+    }
+}
+
+export const initServer = async ()=> {
     const app = express();
 
     try{
+middlewares(app)
+        await conectarDB();
         app.listen(process.env.PORT,()=>{
             console.log(`Servidor corriendoen el puerto ${process.env.PORT} `)
         })
---
+++ configs/db.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import mongoose from 'mongoose'
+
+export const dbConnection = async () =>{
+    try{
+        mongoose.connection.on('error', () =>{
+            console.log('MongoDB  no se pudo conectar a mongoDB')
+        })
+
+        mongoose.connection.on('connecting' , ()=> {
+            console.log('MongoDB intentando conectar a mongoDB')
+        })
+        mongoose.connection.on('connected' , ()=> {
+            console.log('MongoDB conectado a mongoDB')
+        })
+        mongoose.connection.on('open' , ()=> {
+            console.log(' MongoDB conectado a la base de datos')
+        })
+        mongoose.connection.on('reconnected' , ()=> {
+            console.log('MongoDB reconectado a mongoDB')
+        })
+        mongoose.connection.on('disconnecting' , ()=> {
+            console.log('MongoDB desconectado a mongoDB')
+        })
+        await mongoose.connect(process.env.URI_MONGODB, {
+            serverSelectionTimeoutMS: 5000,
+            maxPoolSize: 10
+        })
+    }catch(error){
+        console.log(`Error al conectar la db: ${error}`)
+    }
+}
